c743bedacf6d4e3f9846a5477253a47174f591f6
Waveyyyy <waves-sec@protonmail.com>
2023-11-08 23:50:50 +0000

    Added SSTI vuln on the index page when search is performed

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, render_template_string, url_for, request
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index.html")
 def landing_page():
-    return render_template('index.html')
+    data = None
+    if request.method == 'GET':
+        if request.args.get('q'):
+            data = render_template_string(request.args.get('q'))
+    return render_template('index.html', data=data)
 
 @app.route("/about.html")
 def about_page():
